cb25838fbce02579e79d4213febce6170a9b2cf5
ambika-gits <ambika05012006@gmail.com>
2026-07-01 20:39:12 +0530

    Day 5: String Problems

---
+++ 05_Strings/RemoveSpaces.java
@@ -0,0 +1,11 @@
+public class RemoveSpaces {
+
+    public static void main(String[] args) {
+
+        String str = "Java DSA Challenge";
+
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+    }
+}
