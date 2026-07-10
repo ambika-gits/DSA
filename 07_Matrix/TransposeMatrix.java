42d00d9a1a84a8046ffcd6671ffae51097779244
ambika-gits <ambika05012006@gmail.com>
2026-07-10 22:28:50 +0530

    Day 7: Matrix (2D Arrays)

---
+++ 07_Matrix/TransposeMatrix.java
@@ -0,0 +1,23 @@
+public class TransposeMatrix {
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        System.out.println("Transpose Matrix:");
+
+        for (int i = 0; i < matrix[0].length; i++) {
+
+            for (int j = 0; j < matrix.length; j++) {
+
+                System.out.print(matrix[j][i] + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
