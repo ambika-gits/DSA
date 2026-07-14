466868af30f8fda64d2d65d923e4cad8204e1c20
ambika-gits <ambika05012006@gmail.com>
2026-07-14 23:20:53 +0530

    Day 9: Solved basic String problems in Java

---
+++ 09_Strings/PalindromeString.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String reverse = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+
+        if (str.equals(reverse))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+
+        sc.close();
+    }
+}
---
+++ 09_Strings/RemoveSpaces.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RemoveSpaces {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str = sc.nextLine();
+
+        String result = str.replace(" ", "");
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
